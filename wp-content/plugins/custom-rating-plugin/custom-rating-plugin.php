57f99eda5babad4a6fc8df32893a47f214d764c7
urx2me <122450666+urx2me@users.noreply.github.com>
2024-08-04 16:47:38 +0800

    rating system done

--- wp-content/plugins/custom-rating-plugin/custom-rating-plugin.php
+++ wp-content/plugins/custom-rating-plugin/custom-rating-plugin.php
@@ -8,15 +8,11 @@
 
 // Enqueue scripts and styles
 function crp_enqueue_scripts() {
-    wp_enqueue_script('jquery'); // Ensure jQuery is enqueued
     wp_enqueue_script('crp-script', plugins_url('js/crp-script.js', __FILE__), array('jquery'), '1.0', true);
     wp_localize_script('crp-script', 'crp_ajax', array('ajax_url' => admin_url('admin-ajax.php'), 'nonce' => wp_create_nonce('crp_nonce')));
-    
-    $css_file = plugins_url('css/rating.css', __FILE__);
-    wp_enqueue_style('crp-style', $css_file);
+    wp_enqueue_style('crp-style', plugins_url('css/rating.css', __FILE__), array(), '1.0');
 }
 add_action('wp_enqueue_scripts', 'crp_enqueue_scripts');
-
 
 // Handle AJAX request to submit rating
 function crp_submit_rating() {
@@ -29,7 +25,7 @@ function crp_submit_rating() {
 
     $user_id = get_current_user_id();
     $recipe_id = intval($_POST['recipe_id']);
-    $rating = intval($_POST['rating']);
+    $rating = floatval($_POST['rating']);
 
     $author_id = get_post_field('post_author', $recipe_id);
     if ($author_id == $user_id) {
@@ -37,38 +33,24 @@ function crp_submit_rating() {
         return;
     }
 
-    $user_rating = get_post_meta($recipe_id, 'crp_user_rating_' . $user_id, true);
-    if ($user_rating) {
+    $existing_rating = get_post_meta($recipe_id, 'crp_user_rating_' . $user_id, true);
+    if ($existing_rating) {
         wp_send_json_error('You have already rated.');
         return;
     }
 
-    // Save user rating
-    $all_ratings = get_post_meta($recipe_id, 'crp_ratings', true);
-    if (!is_array($all_ratings)) {
-        $all_ratings = array();
-    }
-    $all_ratings[$user_id] = $rating;
-    update_post_meta($recipe_id, 'crp_ratings', $all_ratings);
     update_post_meta($recipe_id, 'crp_user_rating_' . $user_id, $rating);
 
-    // Calculate new average rating
-    $total_ratings = count($all_ratings);
-    $sum_ratings = array_sum($all_ratings);
-    $average_rating = $total_ratings ? round($sum_ratings / $total_ratings, 1) : 0;
+    $all_ratings = get_post_meta($recipe_id, 'crp_ratings', true);
+    if (!is_array($all_ratings)) {
+        $all_ratings = [];
+    }
+    $all_ratings[] = $rating;
+    update_post_meta($recipe_id, 'crp_ratings', $all_ratings);
 
-    // Update average rating in post meta
-    update_post_meta($recipe_id, '_average_rating', $average_rating);
-
-    wp_send_json_success(array(
-        'average_rating' => $average_rating,
-        'total_ratings' => $total_ratings
-    ));
+    wp_send_json_success('Rating submitted.');
 }
 add_action('wp_ajax_crp_submit_rating', 'crp_submit_rating');
-// add_action('wp_ajax_nopriv_crp_submit_rating', 'crp_submit_rating'); // Allow non-logged-in users to rate if required
-
-
 
 // Display rating stars
 function crp_display_rating($content) {
@@ -76,9 +58,7 @@ function crp_display_rating($content) {
         return $content;
     }
 
-    global $wpdb;
     $recipe_id = get_the_ID();
-
     $all_ratings = get_post_meta($recipe_id, 'crp_ratings', true);
     $total_ratings = is_array($all_ratings) ? count($all_ratings) : 0;
     $sum_ratings = is_array($all_ratings) ? array_sum($all_ratings) : 0;
@@ -87,8 +67,8 @@ function crp_display_rating($content) {
     ob_start();
     ?>
     <div class="crp-rating" data-recipe-id="<?php echo esc_attr($recipe_id); ?>">
-        <div class="crp-stars" data-user-rating="<?php echo esc_attr(get_post_meta($recipe_id, 'crp_user_rating_' . get_current_user_id(), true)); ?>">
-            <?php for ($i = 1; $i <= 5; $i++) : ?>
+        <div class="crp-stars">
+            <?php for ($i = 0.5; $i <= 5; $i += 0.5) : ?>
                 <span class="star" data-value="<?php echo $i; ?>">&#9733;</span>
             <?php endfor; ?>
         </div>
@@ -117,3 +97,4 @@ function crp_display_rating($content) {
     return $content . ob_get_clean();
 }
 add_filter('the_content', 'crp_display_rating');
+?>
--- wp-content/plugins/custom-rating-plugin/js/crp-script.js
+++ wp-content/plugins/custom-rating-plugin/js/crp-script.js
@@ -1,72 +1,24 @@
+//READ THIS FILE IS LOCATED IN THE js FOLDER in custom-rating-plugin FOLDER UNDER PLUGINS IN WP-CONTENT
 jQuery(document).ready(function($) {
-    // Handle star click for rating
     $('.crp-stars .star').on('click', function() {
-        var starValue = $(this).data('value');
+        var rating = $(this).data('value');
         var recipeId = $(this).closest('.crp-rating').data('recipe-id');
 
-        $.ajax({
-            url: crp_ajax.ajax_url,
-            type: 'POST',
-            data: {
+        $.post(
+            crp_ajax.ajax_url,
+            {
                 action: 'crp_submit_rating',
-                nonce: crp_ajax.nonce,
+                rating: rating,
                 recipe_id: recipeId,
-                rating: starValue
+                nonce: crp_ajax.nonce
             },
-            success: function(response) {
+            function(response) {
                 if (response.success) {
-                    alert('Rating submitted successfully.');
-
-                    // Update the average rating and total ratings
-                    var averageRating = response.data.average_rating;
-                    var totalRatings = response.data.total_ratings;
-
-                    $('.crp-average-rating').html('<p>Average Rating: ' + averageRating.toFixed(1) + ' (' + totalRatings + ' ratings)</p>');
-
-                    // Update the stars display based on the average rating
-                    $('.crp-stars .star').each(function(index) {
-                        if (index < Math.floor(averageRating)) {
-                            $(this).addClass('filled');
-                        } else {
-                            $(this).removeClass('filled');
-                        }
-                    });
-
-                    // Update the message
-                    $('.crp-message').html('<p>You have rated</p>');
+                    location.reload();
                 } else {
                     alert(response.data);
                 }
             }
-        });
-    });
-
-    // Star hover effect
-    $('.crp-stars .star').hover(
-        function() {
-            var index = $(this).index();
-            $('.crp-stars .star').each(function(i) {
-                if (i <= index) {
-                    $(this).addClass('hover');
-                } else {
-                    $(this).removeClass('hover');
-                }
-            });
-        }, 
-        function() {
-            $('.crp-stars .star').removeClass('hover');
-        }
-    );
-
-    // Ensure stars remain filled based on the rating after hovering
-    $('.crp-stars .star').on('mouseleave', function() {
-        var averageRating = parseFloat($('.crp-rating').data('average-rating'));
-        $('.crp-stars .star').each(function(index) {
-            if (index < Math.floor(averageRating)) {
-                $(this).addClass('filled');
-            } else {
-                $(this).removeClass('filled');
-            }
-        });
+        );
     });
 });
